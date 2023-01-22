64ee4c356640b12314278ffd255f5ddb0d629118
Gust4voSales <manoel0gustavo@gmail.com>
2023-01-21 23:49:44 -0300

    feat: add list appointments basic route

--- src/infra/http/appointment/dtos/list-appointments-query.ts
+++ src/infra/http/appointment/dtos/list-appointments-query.ts
@@ -2,13 +2,13 @@ import { Type } from "class-transformer"
 import { IsDate, IsDefined, ValidateIf } from "class-validator"
 
 export class ListAppointmentsQuery {
-  @ValidateIf(o => o.endDate) // only validate if both date properties are passed  
+  @ValidateIf(o => o.endDate) // only validate if endDate is also passed
   @IsDefined()
   @IsDate()
   @Type(() => Date)
   startDate: Date
 
-  @ValidateIf(o => o.startDate) // only validate if both date properties are passed 
+  @ValidateIf(o => o.startDate) // only validate if startDate is also passed
   @IsDefined()
   @IsDate()
   @Type(() => Date)
