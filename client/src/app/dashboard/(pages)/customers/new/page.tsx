41c275e7f5b8ea9df55ba76ef2e877c46ca5c31a
Gust4voSales <manoel0gustavo@gmail.com>
2023-02-12 11:50:38 -0300

    fix: properly go back on page title btn

--- client/src/app/dashboard/(pages)/customers/new/page.tsx
+++ client/src/app/dashboard/(pages)/customers/new/page.tsx
@@ -29,7 +29,7 @@ export default function NewCustomer() {
 
   return (
     <div>
-      <PageTitle back title="Novo cliente" />
+      <PageTitle back="/dashboard/customers" title="Novo cliente" />
 
       <CustomersForm onSubmit={handleCreateCustomer} isLoading={createCustomerMutation.isLoading} />
     </div>
