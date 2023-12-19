b4aaf84c1e59f456f6c1a8a7a77712e339e1a4c0
Gust4voSales <manoel0gustavo@gmail.com>
2023-12-19 20:31:18 -0300

    build

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@infra/http/http.module';
 import { AuthModule } from '@infra/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, HttpModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), AuthModule, HttpModule],
 })
 export class AppModule { }
